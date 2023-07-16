595d30d8afdefbc9376f0458369983aed67ad9a7
carlosavellar <carlos.avellar@gmail.com>
2023-07-16 14:40:46 -0300

    productSlice Products

--- src/component/Products/Products.tsx
+++ src/component/Products/Products.tsx
@@ -8,26 +8,25 @@ import {
 } from "../../store/slices/productSlice";
 import mockdata from "./../../mock-data/products.json";
 
-const Products = () => {
+export const Products = () => {
   const products = useSelector(selectAllProducts);
   const dispatch = useDispatch();
+  const [prodUm, setProdUm] = useState<Array<IProductStateItem>>([]);
 
   useEffect(() => {
     dispatch(fetchProduct({ payload: mockdata }));
   }, [mockdata]);
 
   useEffect(() => {
-    console.log(products);
+    setTimeout(() => {
+      console.log(products["payload"]);
+      setProdUm(products);
+    }, 3000);
   }, [products]);
 
   return (
     <>
       <h1>Products</h1>
-      {products.payload.value.map((item: IProductStateItem) => {
-        return <div key={item.id}>{item.name}</div>;
-      })}
     </>
   );
 };
-
-export default Products;
