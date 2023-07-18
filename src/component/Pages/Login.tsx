a54762991523ab8128068563d34d64ced9122158
carlosavellar <carlos.avellar@gmail.com>
2023-07-18 08:43:20 -0300

    _

--- src/component/Pages/Login.tsx
+++ src/component/Pages/Login.tsx
@@ -7,33 +7,19 @@ import "./Login.css";
 function Login() {
   return (
     <div className="login-box">
-      {/* <Container>
-        <Row>
-          <Col> */}
       <Form className="p-5 col-example mh-100 form-login">
         <Form.Group className="mb-3" controlId="formBasicEmail">
           <Form.Label>Email address</Form.Label>
           <Form.Control type="email" placeholder="Enter email" />
-          {/* <Form.Text className="text-muted">
-          We'll never share your email with anyone else.
-        </Form.Text> */}
         </Form.Group>
         <Form.Group className="mb-3" controlId="formBasicPassword">
           <Form.Label>Password</Form.Label>
           <Form.Control type="password" placeholder="Password" />
         </Form.Group>
-        <Button
-          variant="primary"
-          type="submit"
-          className="align-items-end"
-          disabled
-        >
+        <Button variant="primary" type="submit" className="align-items-end">
           Entrar
         </Button>
       </Form>
-      {/* </Col>
-        </Row>
-      </Container> */}
     </div>
   );
 }
